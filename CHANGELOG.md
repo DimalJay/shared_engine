088012f5264e6fe4799535638a90ea621c699291
DimalJay <dimalkavindujay@gmail.com>
2024-02-28 15:50:46 +0530

    add timestamps

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.9
+
+- Add Timestamp to Model
+
 ## 1.0.0
 
 - Initial version.
--- lib/src/models/model_abstract.dart
+++ lib/src/models/model_abstract.dart
@@ -1,8 +1,19 @@
 abstract class TxModel {
   final String id;
   final String collectionName;
+  final DateTime createdAt;
+  final DateTime updatedAt;
 
-  TxModel(this.collectionName, {required this.id});
+  TxModel(
+    this.collectionName, {
+    required this.id,
+    DateTime? createdAt,
+    DateTime? updatedAt,
+  })  : createdAt = createdAt ?? DateTime.now(),
+        updatedAt = updatedAt ?? DateTime.now();
 
-  Map<String, dynamic> toJson();
+  Map<String, dynamic> toJson() => {
+        'createdAt': createdAt.toIso8601String(),
+        'updatedAt': updatedAt.toIso8601String(),
+      };
 }
